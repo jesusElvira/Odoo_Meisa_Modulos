ee2662ac7c5570091a6bf6f7c4d0900e450b63fd
Odoo <odoo@odoo.meisa-e.com>
2017-05-19 16:38:34 +0000

    Ampliacion del modulo herencia de proveedores

--- herencia_res_partner/models/herencia_res_partner.py
+++ herencia_res_partner/models/herencia_res_partner.py
@@ -5,6 +5,13 @@ from datetime import datetime
 
 class herencia_res_partner(models.Model): 
     _inherit = "res.partner" 
-    codigo = fields.Integer(string='codigo', required=True)
-    fecha_alta = fields.Date('Fecha alta') 
+    codigo = fields.Integer(string='codigo', required=True) 
+ 
+    fecha_alta = fields.Datetime(string='fecha_alta', required=True) 
+ 
+    fecha_baja = fields.Datetime(string='fecha_baja', required=False) 
+ 
+    fecha_evaluacion = fields.Datetime(string='fecha_evaluacion', required=False) 
+ 
+    numero_incidencias = fields.Integer(string='numero_incidencias', required=True) 
  
